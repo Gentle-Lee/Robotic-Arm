b3d4a4c7dc4227b7cd21bc9aa9b8c0f4c741000a
Gentle-Lee <tommy007lee@gmail.com>
2019-01-15 18:52:52 +0800

    [fix]windows handle id varied

--- CppHttpServerDemo/CppHttpServerDemo/httpserver/main.cpp
+++ CppHttpServerDemo/CppHttpServerDemo/httpserver/main.cpp
@@ -4,6 +4,10 @@
 #include <cstring>
 #include <string>
 #include <tchar.h>
+#include <set>
+#include <map>
+
+
 using namespace std;
 // 初始化HttpServer静态类成员
 mg_serve_http_opts HttpServer::s_server_option;
@@ -11,8 +15,12 @@ std::string HttpServer::s_web_dir = "./web";
 std::unordered_map<std::string, ReqHandler> HttpServer::s_handler_map;
 std::unordered_set<mg_connection *> HttpServer::s_websocket_session_set;
 
-const string operationStrings[9] = {"x%2B","x-","x停","y%2B","y-","y停" ,"z%2B","z-","z停" };
-const int operationHandleId[9] = {0x000206FC,0x000206FA,0x0002070A,0x000206EE,0x000206F6,0x00020706,0x00020700,0x000206F4,0x0002070C};
+
+
+//机械臂参数
+set<string> stringsSet;
+map<string,int> operationHandleMap;
+const string operationStrings[] = {"示教","直角","x+","x-","x停","y+","y-","y停" ,"z+","z-","z停" };
 
 bool handle_fun1(std::string url, std::string body, mg_connection *c, OnRspCallback rsp_callback)
 {
@@ -44,34 +52,27 @@ void sendMessageToMachine(string body) {
 	//解析请求字符串
 	int index = body.find('=');
 	string value = body.substr(index+1, body.length()-1);
-	cout << "value = " << value << endl;
-	int operationHandleIndex = 0;
-	int stopHandleIndex = 0;
-	for (int  i = 0; i < 9; i++){
-		if (value.compare(operationStrings[i]) == 0){
-			operationHandleIndex = i;
-			stopHandleIndex = (i / 3) * 3 + 2;
-			cout << "operationHandleIndex = "<< operationHandleIndex <<" stop = " << stopHandleIndex << endl;
-			break;
-		}
+	string axis = value.substr(0, 1);
+	string operationStr = value.substr(1);
+	if (operationStr.compare("%2B") == 0){
+		value.replace(1, 3, "+");
 	}
-	cout << "start = " << operationHandleId[operationHandleIndex] << " stop = " << operationHandleId[stopHandleIndex] << endl;
-	//以下为操控windows窗体程序
-	const char* param = "WindowsForms10.Window.8.app.0.33c0d9d";
-	//第一个参数是Windows窗体的窗体类，第二个参数是窗体的标题。不熟悉windows编程的需要先找一些Windows窗体数据结构的知识来看看，还有windows消息循环处理，其他的东西不用看太多。
-	HWND main = FindWindow("WindowsForms10.Window.8.app.0.33c0d9d", "yj");
-	SetForegroundWindow(main);
+	string stop = string(axis);
+	stop.append("停");
+	cout << "value = " << value << " stop = " << stop << " handle hwnd = " << operationHandleMap[value] << " stop hwnd = "<< operationHandleMap[stop]<< endl;
+	//cout << " shijiao = " << operationHandleMap[operationStrings[0]] << " zhijiao = " << operationHandleMap[operationStrings[1]] << endl;
+	HWND yj = FindWindow(NULL, "yj");
 	//Sleep(500);
 	//下载
-	HWND panel = ::GetDlgItem(main, 0x00000002);
-	HWND shijiao = ::GetDlgItem(panel, 0x001F0A20);
-	HWND panel2 = ::GetDlgItem(shijiao, 0x00000003);
-	HWND zhijiao = ::GetDlgItem(panel2, 0x00420A44);
-	HWND operaion = ::GetDlgItem(zhijiao, operationHandleId[operationHandleIndex]);
-	SendMessage(operaion, BM_CLICK, 0, 0);
+	HWND firstPanel = ::GetDlgItem(yj, 0x00000002);
+	HWND shijiao = ::GetDlgItem(firstPanel, operationHandleMap[operationStrings[0]]);
+	HWND secondPanel = ::GetDlgItem(shijiao, 0x00000003);
+	HWND zhijiao = ::GetDlgItem(secondPanel, operationHandleMap[operationStrings[1]]);
+	HWND operation = ::GetDlgItem(zhijiao, operationHandleMap[value.c_str()]);
+	SendMessage(operation, BM_CLICK, 0, 0);
 	Sleep(200);
-	HWND stop = ::GetDlgItem(zhijiao, operationHandleId[stopHandleIndex]);
-	SendMessage(stop, BM_CLICK, 0, 0);
+	HWND stopOperation = ::GetDlgItem(zhijiao, operationHandleMap[stop.c_str()]);
+	SendMessage(stopOperation, BM_CLICK, 0, 0);
 	return;
 }
 
@@ -85,20 +86,69 @@ bool handleRoboticArmMovement(std::string url, std::string body, mg_connection *
 	return true;
 }
 
-void startDrill() {
-	HWND main = FindWindow(NULL, _T("yj"));
-	HWND pDSubWnd = ::GetDlgItem(main, 0x0002);
-	HWND pDSsubWnd = FindWindowEx(pDSubWnd, NULL, "WindowsForms10.Window.8.app.0.378734a", "输出");
-	ShowWindow(pDSsubWnd, 9);
-	HWND pDSsssubWnd = FindWindowEx(pDSsubWnd, NULL, "WindowsForms10.BUTTON.app.0.378734a", "o08");
-	SendMessage(pDSsssubWnd, BM_CLICK, 0, 0);
-	std::cout << "drill start" << endl;
+int Pnum = 0, Cnum;//父窗口数量，每一级父窗口的子窗口数量  
+
+//---------------------------------------------------------  
+//EnumChildWindows回调函数，hwnd为指定的父窗口  
+//---------------------------------------------------------  
+BOOL CALLBACK EnumChildWindowsProc(HWND hWnd, LPARAM lParam)
+{
+	char WindowTitle[100] = { 0 };
+	Cnum++;
+	::GetWindowText(hWnd, WindowTitle, 100);
+	if (stringsSet.find(WindowTitle) != stringsSet.end()){
+		printf("%s\n", WindowTitle);
+		operationHandleMap[WindowTitle] = (int)hWnd;
+		cout << "hWnd = " << hWnd << endl;
+	}
+	return true;
+}
+//---------------------------------------------------------  
+//EnumWindows回调函数，hwnd为发现的顶层窗口  
+//---------------------------------------------------------  
+BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam)
+{
+	if (GetParent(hWnd) == NULL && IsWindowVisible(hWnd))  //判断是否顶层窗口并且可见  
+	{
+		Pnum++;
+		Cnum = 0;
+		char WindowTitle[100] = { 0 };
+		::GetWindowText(hWnd, WindowTitle, 100);
+		/*printf("-------------------------------------------\n");
+		printf("%d: %s\n", Pnum, WindowTitle);*/
+		string targetParentTitle = "yj";
+		if (strcmp(WindowTitle, targetParentTitle.c_str()) == 0) {
+			cout << "Find Main Window yj" << endl;
+			EnumChildWindows(hWnd, EnumChildWindowsProc, NULL); //获取父窗口的所有子窗口  
+		}
+	}
+	return true;
+}
+
+
+bool getHandle() {
+	for (int i = 0; i < 11; i++) {
+		stringsSet.insert(operationStrings[i]);
+	}
+	//获取屏幕上所有的顶层窗口,每发现一个窗口就调用回调函数一次  
+	EnumWindows(EnumWindowsProc, NULL);
+	if (operationHandleMap.empty()){
+		cout << "get handle error" << endl;
+		return false;
+	}
+	else {
+		cout << "get handle success" << endl;
+		return true;
+	}
 }
 
 int main(int argc, char *argv[]) 
 {
 	std::string port = "7999";
 	auto http_server = std::shared_ptr<HttpServer>(new HttpServer);
+	if (!getHandle()){
+		return 0;
+	}
 	http_server->Init(port);
 	//start drill
 	//startDrill();
@@ -106,6 +156,7 @@ int main(int argc, char *argv[])
 	http_server->AddHandler("/api/fun1", handle_fun1);
 	http_server->AddHandler("/api/fun2", handle_fun2);
 	http_server->AddHandler("/api/movement", handleRoboticArmMovement);
+
 	http_server->Start();
 	
 
